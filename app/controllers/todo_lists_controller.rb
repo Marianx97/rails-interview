8a52bea05e988263b5ffaa789fe12a837e5c4647
Mariano Ranucci <ranucci.mariano@gmail.com>
2024-03-17 10:52:11 -0300

    Added full CRUD endpoints for todo list model (non-api)

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -1,4 +1,6 @@
 class TodoListsController < ApplicationController
+  before_action :set_todo_list, only: %i[edit update show destroy]
+
   # GET /todolists
   def index
     @todo_lists = TodoList.all
@@ -6,10 +8,58 @@ class TodoListsController < ApplicationController
     respond_to :html
   end
 
+  # POST /todolists
+  def create
+    @todo_list = TodoList.new(todo_list_params)
+
+    if @todo_list.save
+      redirect_to todo_lists_path, notice: 'List successfully created!'
+    else
+      flash.now[:alert] = 'Error creating list'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   # GET /todolists/new
   def new
     @todo_list = TodoList.new
 
     respond_to :html
+  end
+
+  # GET /todolists/:id/edit
+  def edit
+    respond_to :html
+  end
+
+  # GET /todolists/:id
+  def show
+    respond_to :html
+  end
+
+  # PUT /todolists/:id
+  def update
+    if @todo_list.update(todo_list_params)
+      redirect_to todo_list_path, notice: 'List successfully updated!'
+    else
+      flash.now[:alert] = 'Error updating list'
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /todolists/:id
+  def destroy
+    @todo_list.destroy
+    redirect_to todo_lists_path, notice: 'List successfully deleted!', status: :see_other
+  end
+
+  private
+
+  def set_todo_list
+    @todo_list ||= TodoList.find(params[:id])
+  end
+
+  def todo_list_params
+    params.require(:todo_list).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  root to: 'todo_lists#index'
+
   namespace :api do
     resources :todo_lists, only: %i[index show create update destroy], path: :todolists do
       resources :todo_lists_items, only: %i[index show create update destroy], path: :items
     end
   end
 
-  resources :todo_lists, only: %i[index new], path: :todolists
+  resources :todo_lists, path: :todolists
 end
